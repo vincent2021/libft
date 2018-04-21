57b6d9d2763c4488aebfbca77e6d23d5c1f8b341
Vincent MUCCHIELLI <vimucchi@student.42.fr>
2018-04-22 00:30:38 +0200

    done?

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: vimucchi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/08 22:29:47 by vimucchi          #+#    #+#             */
-/*   Updated: 2018/04/22 00:09:49 by vimucchi         ###   ########.fr       */
+/*   Updated: 2018/04/22 00:22:23 by vimucchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ char	*ft_strtrim(char const *s)
 	size_t	i;
 	size_t	l;
 	char	*str;
+	char	*tmp;
 
+	str = malloc(1252828);
 	if (!s)
 		return (NULL);
 	if ((l = ft_strlen(s)) == 0)
@@ -29,7 +31,8 @@ char	*ft_strtrim(char const *s)
 		return ("");
 	while (s[l] == ' ' || s[l] == '\n' || s[l] == '\t' || s[l] == '\0')
 		l--;
-	if (!(str = ft_strsub(s, i, l - i + 1)))
+	if (!(tmp = ft_strsub(s, i, l - i + 1)))
 		return (NULL);
+	str = tmp;
 	return (str);
 }
